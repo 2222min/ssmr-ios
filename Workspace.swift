5c2c6bfd129694a36732da61cebae1afff3d14f9
이민철 <mincheol@finda.co.kr>
2022-09-10 21:17:52 +0900

    workspace 추가

---
+++ Workspace.swift
@@ -0,0 +1,10 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by mincheol on 2022/09/10.
+//
+
+import ProjectDescription
+
+let workspace = Workspace(name: "SSMR", projects: ["Projects/*"])
