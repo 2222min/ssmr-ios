abf0f900d0797ee9ba43100b4e9d255f8ba4a859
이민철 <mincheol@finda.co.kr>
2022-09-24 17:10:52 +0900

    feat: BaseViewController 백그라운드 칼라 변경 feat: image + label 형식의 custom buttom 추가

---
+++ Projects/CommonUI/Sources/CustomUI/LeftImageButton.swift
@@ -0,0 +1,56 @@
+//
+//  LeftImageButton.swift
+//  CommonUI
+//
+//  Created by mincheol on 2022/09/24.
+//  Copyright © 2022 team.io. All rights reserved.
+//
+
+import UIKit
+
+public final class LeftImageButton: UIButton {
+    public var normalTitle: NSAttributedString = .init() {
+        didSet {
+            self.setAttributedTitle(self.normalTitle, for: .normal)
+            setNeedsLayout()
+        }
+    }
+    
+    public var selectedTitle: NSAttributedString = .init() {
+        didSet {
+            self.setAttributedTitle(self.selectedTitle, for: .selected)
+            setNeedsLayout()
+        }
+    }
+    
+    public var normalImage: UIImage = .init() {
+        didSet {
+            self.setImage(self.normalImage, for: .normal)
+            setNeedsLayout()
+        }
+    }
+    
+    public var seletedImage: UIImage = .init() {
+        didSet {
+            self.setImage(self.seletedImage, for: .selected)
+            setNeedsLayout()
+        }
+    }
+    
+    public init() {
+        super.init(frame: .zero)
+        self.backgroundColor = .clear
+        self.imageEdgeInsets = .init(top: 0, left: 0, bottom: 0, right: 0)
+        self.titleEdgeInsets = .init(top: 0, left: 8, bottom: 0, right: 0)
+        self.imageView?.contentMode = .scaleAspectFit
+        self.titleLabel?.lineBreakMode = .byWordWrapping
+        self.contentHorizontalAlignment = .leading
+        self.contentVerticalAlignment = .top
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
