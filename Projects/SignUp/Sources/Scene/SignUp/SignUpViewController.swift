346f3d2afbabf823cb8e2ec34b0dc063fd7d5076
JEONGKUNHO <dghj6739@gmail.com>
2022-09-29 23:59:15 +0900

    아이디, 패스워드 텍스트필드 추가 및 중복확인 버튼 구현 ~ing

--- Projects/SignUp/Sources/Scene/SignUp/SignUpViewController.swift
+++ Projects/SignUp/Sources/Scene/SignUp/SignUpViewController.swift
@@ -32,6 +32,35 @@ class SignUpViewController: BaseViewController {
         $0.numberOfLines = 0
     }
     
+    private let idTextField = UnderlineTextFieldWithTitle().then {
+        $0.title.attributedText = "아이디".styled(
+            typo: .DDaengH3,
+            byAdding: [.color(CommonUIAsset.blackGrey.color)])
+        $0.textField.attributedPlaceholder = NSAttributedString(
+            string: "아이디를 입력해 주세요",
+            attributes: [NSAttributedString.Key.foregroundColor : CommonUIAsset.whiteGrey.color]
+        )
+    }
+    
+    private let duplicationButton = UIButton().then {
+        $0.setTitle("중복확인", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = CommonUIAsset.grey.color
+        $0.layer.cornerRadius = 32
+    }
+    
+    private let pwTextField = UnderlineTextFieldWithTitle().then {
+        $0.title.attributedText = "비밀번호".styled(
+            typo: .DDaengH3,
+            byAdding: [.color(CommonUIAsset.blackGrey.color)])
+        $0.textField.attributedPlaceholder =  NSAttributedString(
+            string: "비밀번호를 입력해 주세요.",
+            attributes: [NSAttributedString.Key.foregroundColor : CommonUIAsset.whiteGrey.color]
+        )
+        $0.textField.isSecureTextEntry = true
+    }
+    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -41,6 +70,9 @@ class SignUpViewController: BaseViewController {
     override func configureUI() {
         self.view.addSubview(signUpLabel)
         self.view.addSubview(guideLabel)
+        self.view.addSubview(idTextField)
+        self.view.addSubview(duplicationButton)
+        self.view.addSubview(pwTextField)
     }
     
     override func setupConstraints() {
@@ -52,5 +84,22 @@ class SignUpViewController: BaseViewController {
             $0.top.equalTo(self.signUpLabel.snp.bottom).offset(16)
             $0.leading.equalToSuperview().offset(16)
         }
+        self.idTextField.snp.makeConstraints {
+            $0.top.equalTo(self.guideLabel.snp.bottom).offset(40)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(73)
+        }
+        // TODO: 중복 버튼 만들기
+        self.duplicationButton.snp.makeConstraints {
+            $0.top.bottom.equalTo(self.idTextField.textField)
+            $0.trailing.equalTo(self.idTextField.snp.trailing)
+            $0.width.equalTo(72)
+        }
+        self.pwTextField.snp.makeConstraints {
+            $0.top.equalTo(self.idTextField.snp.bottom).offset(42)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(73)
+        }
+        
     }
 }
