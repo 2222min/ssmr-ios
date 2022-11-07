8eca97dae0aeaec315981af4b8e9c47a2866854a
JEONGKUNHO <dghj6739@gmail.com>
2022-11-07 23:26:39 +0900

    사업자 등록 페이지 1 UI 구현 중

---
+++ Projects/CommonUI/Sources/CustomUI/RightImageButton.swift
@@ -0,0 +1,71 @@
+//
+//  RightImageButton.swift
+//  CommonUI
+//
+//  Created by 정건호 on 2022/11/06.
+//  Copyright © 2022 team.io. All rights reserved.
+//
+
+import UIKit
+
+/*
+ 왼쪽에 이미지 오른쪽에 라벨이 있는 UI
+ 
+ Ex) let button = RightImageButton().then {
+  Propertis Setting:
+    Button State - Normal
+        $0.normalTitle = NSAttributedString()
+        $0.normalImage = UIImage()
+    Button State - Selected
+        $0.selectedTitle = NSAttributedString()
+        $0.seletedImage = UIImage()
+ }
+ */
+
+public final class RightImageButton: UIButton {
+    public var normalTitle: NSAttributedString = .init() {
+        didSet {
+            self.setAttributedTitle(self.normalTitle, for: .normal)
+            setNeedsLayout()
+        }
+    }
+    
+    public var selectedTitle: NSAttributedString = .init() {
+        didSet {
+            self.setAttributedTitle(self.selectedTitle, for: .selected)
+            setNeedsLayout()
+        }
+    }
+    
+    public var normalImage: UIImage = .init() {
+        didSet {
+            self.setImage(self.normalImage, for: .normal)
+            setNeedsLayout()
+        }
+    }
+    
+    public var selectedImage: UIImage = .init() {
+        didSet {
+            self.setImage(self.selectedImage, for: .selected)
+            setNeedsLayout()
+        }
+    }
+    
+    public init() {
+        super.init(frame: .zero)
+        self.backgroundColor = .clear
+        self.imageEdgeInsets = .init(top: 0, left: 0, bottom: 0, right: 0)
+        self.titleEdgeInsets = .init(top: 0, left: 8, bottom: 0, right: 0)
+        self.imageView?.contentMode = .scaleAspectFit
+        self.titleLabel?.lineBreakMode = .byTruncatingTail
+        self.titleLabel?.numberOfLines = 0
+        self.contentHorizontalAlignment = .leading
+        self.contentVerticalAlignment = .top
+        self.semanticContentAttribute = .forceRightToLeft
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
--- Projects/RegisterBusiness/Sources/Scene/RegiBusinessOneViewController.swift
+++ Projects/RegisterBusiness/Sources/Scene/RegiBusinessOneViewController.swift
@@ -6,24 +6,256 @@
 //
 
 import UIKit
+import CommonUI
 
-class RegiBusinessOneViewController: UIViewController {
+class RegiBusinessOneViewController: BaseViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        view.backgroundColor = .red
+    // MARK: Constants
+    private enum Constants {
+        static let titleLabelText = "사업자 등록하기"
+        static let subTitleLabeltext = "입점을 위해\n사업자 등록 정보를 입력해주세요".styled(
+            typo: .DDaengB1,
+            byAdding: [.color(CommonUIAsset.deepGrey.color)]
+        )
+        static let businessNumberlabelText = "사업자구분 및 번호".styled(
+            typo: .DDaengH3,
+            byAdding: [.color(CommonUIAsset.deepGrey.color)]
+        )
+        static let individualButtonText = "개인".styled(
+            typo: .ButtomSmall,
+            byAdding: [.color(CommonUIAsset.whiteGrey.color)]
+        )
+        static let individualButtonSelectedText = "개인".styled(
+            typo: .ButtomSmall,
+            byAdding: [.color(CommonUIAsset.pointColor.color)]
+        )
+        static let corporationButtonText = "법인".styled(
+            typo: .ButtomSmall,
+            byAdding: [.color(CommonUIAsset.whiteGrey.color)]
+        )
+        static let corporationButtonSelectedText = "법인".styled(
+            typo: .ButtomSmall,
+            byAdding: [.color(CommonUIAsset.pointColor.color)]
+        )
+        static let businessNumberTextFieldPlaceHolder = "businessNumberTextField".styled(
+            typo: .DDaengMB2,
+            byAdding: [.color(CommonUIAsset.whiteGrey.color)]
+        )
+        static let checkButtonText = "확인".styled(
+            typo: .ButtomSmall,
+            byAdding: [.color(CommonUIAsset.white.color)]
+        )
+        static let checkBusinessNumberText = "사업자번호 확인".styled(
+            typo: .DDaengC1,
+            byAdding: [.color(CommonUIAsset.grey.color)]
+        )
+        static let checkBusinessNumberSelectedText = "사업자번호 확인".styled(
+            typo: .DDaengC1,
+            byAdding: [.color(CommonUIAsset.pointColor.color)]
+        )
+        static let businessCategorylabelText = "업태/업종".styled(
+            typo: .DDaengH3,
+            byAdding: [.color(CommonUIAsset.deepGrey.color)]
+        )
+        static let largeCategoryText = "대분류".styled(
+            typo: .DDaengMB2,
+            byAdding: [.color(CommonUIAsset.whiteGrey.color)]
+        )
+        static let largeCategorySelectedText = "대분류".styled(
+            typo: .DDaengMB2,
+            byAdding: [.color(CommonUIAsset.pointColor.color)]
+        )
+        static let mediumCategoryText = "중분류".styled(
+            typo: .DDaengMB2,
+            byAdding: [.color(CommonUIAsset.whiteGrey.color)]
+        )
+        static let mediumCategorySelectedText = "중분류".styled(
+            typo: .DDaengMB2,
+            byAdding: [.color(CommonUIAsset.pointColor.color)]
+        )
+        static let businessNameTextFieldTitle = "상호명".styled(
+            typo: .DDaengH3,
+            byAdding: [.color(CommonUIAsset.deepGrey.color)]
+        )
+        static let businessNameTextFieldPlaceHolder = "상호명(법인명)을 입력해 주세요".styled(
+            typo: .DDaengMB2,
+            byAdding: [.color(CommonUIAsset.whiteGrey.color)]
+        )
+        static let nextButtonText = "다음".styled(
+            typo: .ButtonLarge,
+            byAdding: [.color(CommonUIAsset.white.color)]
+        )
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: Properties
+    
+    // MARK: UI Properties
+    
+    private let titleLabel = UnderlineLabel().then {
+        $0.labelText = Constants.titleLabelText
     }
-    */
-
+    private let subTitleLabel = UILabel().then {
+        $0.attributedText = Constants.subTitleLabeltext
+        $0.numberOfLines = 0
+    }
+    private let businessNumberLabel = UILabel().then {
+        $0.attributedText = Constants.businessNumberlabelText
+    }
+    private let guideImageView = UIImageView().then {
+        $0.image = UIImage(asset: CommonUIAsset.informationMarkBold)
+    }
+    private let buttonStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 16
+        $0.distribution = .fillEqually
+    }
+    private let individualButton = UIButton().then {
+        $0.setAttributedTitle(Constants.individualButtonText, for: .normal)
+        $0.setAttributedTitle(Constants.individualButtonSelectedText, for: .selected)
+        $0.layer.cornerRadius = 8
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor(asset: CommonUIAsset.whiteGrey)?.cgColor
+    }
+    private let corporationButton = UIButton().then {
+        $0.setAttributedTitle(Constants.corporationButtonText, for: .normal)
+        $0.setAttributedTitle(Constants.corporationButtonSelectedText, for: .selected)
+        $0.layer.cornerRadius = 8
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor(asset: CommonUIAsset.whiteGrey)?.cgColor
+    }
+    private let businessNumberTextField = UnderlineTextField().then {
+        $0.textField.attributedPlaceholder = NSAttributedString(
+            string: "사업자번호를 입력해 주세요",
+            attributes: [NSAttributedString.Key.foregroundColor : CommonUIAsset.whiteGrey.color]
+        )
+    }
+    private let checkButton = UIButton().then {
+        $0.setAttributedTitle(Constants.checkButtonText, for: .normal)
+        $0.setBackgroundColor(color: CommonUIAsset.grey.color, forState: .normal)
+        $0.setBackgroundColor(color: CommonUIAsset.pointColor.color, forState: .selected)
+        $0.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 2, right: 0)
+        $0.layer.cornerRadius = 12
+    }
+    private let checkBusinessNumber = LeftImageButton().then {
+        $0.normalImage = UIImage(asset: CommonUIAsset.disapproveImage) ?? UIImage()
+        $0.normalTitle = Constants.checkBusinessNumberText
+        $0.selectedImage = UIImage(asset: CommonUIAsset.approveImage) ?? UIImage()
+        $0.selectedTitle = Constants.checkBusinessNumberSelectedText
+        $0.imageEdgeInsets = .init(top: 3, left: 0, bottom: 0, right: 0)
+        $0.titleEdgeInsets = .init(top: 0, left: 4, bottom: 0, right: -4)
+    }
+    private let businessCategoryLabel = UILabel().then {
+        $0.attributedText = Constants.businessCategorylabelText
+    }
+    private let largeCategory = LabelWithCategory().then {
+        $0.category.setAttributedTitle(Constants.largeCategoryText, for: .normal)
+        $0.category.setAttributedTitle(Constants.largeCategorySelectedText, for: .selected)
+    }
+    private let mediumCategory = LabelWithCategory().then {
+        $0.category.setAttributedTitle(Constants.mediumCategoryText, for: .normal)
+        $0.category.setAttributedTitle(Constants.mediumCategorySelectedText, for: .selected)
+    }
+    private let businessNameTextField = UnderlineTextFieldWithTitle().then {
+        $0.title.attributedText = Constants.businessNameTextFieldTitle
+        $0.textField.attributedPlaceholder = Constants.businessNameTextFieldPlaceHolder
+    }
+    private let nextButton = CTAButton().then {
+        $0.setAttributedTitle(
+            Constants.nextButtonText,
+            for: .normal
+        )
+        $0.isEnabled = false
+    }
+    
+    // MARK: View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func configureUI() {
+        [
+            self.individualButton,
+            self.corporationButton
+        ]
+            .forEach(self.buttonStackView.addArrangedSubview)
+        [
+            self.titleLabel,
+            self.subTitleLabel,
+            self.businessNumberLabel,
+            self.guideImageView,
+            self.buttonStackView,
+            self.businessNumberTextField,
+            self.checkButton,
+            self.checkBusinessNumber,
+            self.businessCategoryLabel,
+            self.largeCategory,
+            self.mediumCategory,
+            self.businessNameTextField,
+            self.nextButton
+        ]
+            .forEach(self.view.addSubview)
+    }
+    
+    // MARK: Constraints
+    override func setupConstraints() {
+        self.titleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(32)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        self.subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        self.businessNumberLabel.snp.makeConstraints {
+            $0.top.equalTo(self.subTitleLabel.snp.bottom).offset(40)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        self.guideImageView.snp.makeConstraints {
+            $0.centerY.equalTo(self.businessNumberLabel).offset(2)
+            $0.leading.equalTo(self.businessNumberLabel.snp.trailing).offset(10)
+        }
+        self.buttonStackView.snp.makeConstraints {
+            $0.top.equalTo(self.businessNumberLabel.snp.bottom).offset(12)
+            $0.height.equalTo(32)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+        self.businessNumberTextField.snp.makeConstraints {
+            $0.top.equalTo(self.buttonStackView.snp.bottom).offset(24)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(24)
+        }
+        self.checkButton.snp.makeConstraints {
+            $0.top.equalTo(self.businessNumberTextField.snp.top)
+            $0.height.equalTo(self.businessNumberTextField.textField)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.width.equalTo(51)
+        }
+        self.checkBusinessNumber.snp.makeConstraints {
+            $0.top.equalTo(self.businessNumberTextField.underline.snp.bottom).offset(12)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        self.businessCategoryLabel.snp.makeConstraints {
+            $0.top.equalTo(self.checkBusinessNumber.snp.bottom).offset(32)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        self.largeCategory.snp.makeConstraints {
+            $0.top.equalTo(self.businessCategoryLabel.snp.bottom).offset(16)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(24)
+        }
+        self.mediumCategory.snp.makeConstraints {
+            $0.top.equalTo(self.largeCategory.snp.bottom).offset(24)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(24)
+        }
+        self.businessNameTextField.snp.makeConstraints {
+            $0.top.equalTo(self.mediumCategory.snp.bottom).offset(32)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(73)
+        }
+        self.nextButton.snp.makeConstraints {
+            $0.top.equalTo(self.businessNameTextField.textField.snp.bottom).offset(48)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+    }
 }
