752eeb68d766fac2d6786832e85e85b3840cc781
JEONGKUNHO <dghj6739@gmail.com>
2023-05-11 23:24:21 +0900

    feat: Kingfisher 라이브러리 추가

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -19,7 +19,8 @@ let dependencies = Dependencies(
             .remote(url: "https://github.com/Rightpoint/BonMot", requirement: .upToNextMinor(from: "6.1.1")),
             .remote(url: "https://github.com/2222min/ReusableKit", requirement: .branch("master")),
             .remote(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", requirement: .upToNextMajor(from: "5.0")),
-            .remote(url: "https://github.com/Swinject/Swinject.git", requirement: .upToNextMajor(from: "2.8.0"))
+            .remote(url: "https://github.com/Swinject/Swinject.git", requirement: .upToNextMajor(from: "2.8.0")),
+            .remote(url: "https://github.com/onevcat/Kingfisher", requirement: .upToNextMajor(from: "7.0.0"))
          ],
          productTypes: [
             "ReactorKit": .framework,
